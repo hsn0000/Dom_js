3fd401a1c63a5d6fd39ca97f4c0de7cddd9f38ba
Husin <Muhamad.husin0412@gmail.com>
2019-11-26 09:49:02 +0700

    traversal,

--- script/traversal.js
+++ script/traversal.js
@@ -13,7 +13,7 @@
 // Dom Traversal
 
 const close = document.querySelectorAll('.close');
-const card = document.querySelectorAll('.card');
+// const card = document.querySelectorAll('.card');
 
 // for( let i = 0; i < close.length; i++ ) {
 //    close[i].addEventListener('click', function(e) {
@@ -26,5 +26,14 @@ const card = document.querySelectorAll('.card');
 close.forEach(function(el) {
     el.addEventListener('click', function(e) {
         e.target.parentElement.style.display = 'none';
+        e.preventDefault();
+        e.stopPropagation();
+    });
+});   
+
+const card = document.querySelectorAll('.card');
+cards.forEach(function(card) {
+    card.addEventListener('click', function(e) {
+       alert('ok');
     });
 });
